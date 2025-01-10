37f535fc52869ac1facdf61fdeb06dbe032e7248
eswar-prog <massraja18@gmail.com>
2025-01-10 12:34:30 +0530

    first commit

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://back-test-production-708f.up.railway.app" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://back-test-production-ab3a.up.railway.app/" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
